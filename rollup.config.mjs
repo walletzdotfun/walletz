33ba1ebff31f0eb9b62e089e53c2d6bf09daca8d
Abdullah1738 <admin@walletz.fun>
2025-02-05 00:12:58 +0400

    chore: rollup fixes

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
+import postcss from 'rollup-plugin-postcss'
 
 export default {
   input: 'src/index.ts',
@@ -24,6 +25,7 @@ export default {
     typescript({
       tsconfig: './tsconfig.json'
     }),
+    postcss(),
     terser()
   ]
 };
