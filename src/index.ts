2a761e987137516df9c7b02f22011243046c2b76
Abdullah1738 <admin@walletz.fun>
2025-02-04 23:38:58 +0400

    Create index.ts

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { WalletzProvider } from './WalletzProvider';
+export { useWalletz } from './useWalletz';
+export { WalletzModal } from './components/WalletzModal';
+export { WalletzButton } from './components/WalletzButton';
+
+export * from './types';
