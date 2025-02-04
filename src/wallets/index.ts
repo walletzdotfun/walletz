91f4495305b24201d598d060c510a7cf17260659
Abdullah1738 <admin@walletz.fun>
2025-02-04 23:56:46 +0400

    feat: update ALL_WALLETS export

--- src/wallets/index.ts
+++ src/wallets/index.ts
@@ -1,4 +1,6 @@
 import { PhantomAdapter } from './phantom';
 import { SolflareAdapter } from './solflare';
+import { GlowAdapter } from './glow';
+import { BackpackAdapter } from './backpack';
 
-export const ALL_WALLETS = [PhantomAdapter, SolflareAdapter];
+export const ALL_WALLETS = [PhantomAdapter, SolflareAdapter, GlowAdapter, BackpackAdapter];
